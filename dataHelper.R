694a39a2709aa7be2778181e54193eb5db5bbdc3
Victor Naidu <vnaidu114@gmail.com>
2016-07-16 00:37:08 -0700

    Change filepaths values to check from static strings to vars
    This allows a user to specify the the path to data and zip file explictly.

--- dataHelper.R
+++ dataHelper.R
@@ -1,16 +1,52 @@
 # Purpose: to ensure dataset avaliabity for plot*.R scripts
 # This is a data helper script that checks if the household power consumption
-# dataset exists. If the dataset dosen't exist, check if the source '.zip' file
-# exists (download if nonexistent) and unzip it.
+# dataset exists. If the dataset dosen't exist, check if the source Zip
+# archive file exists (download if nonexistent) and unzip it.
 
-datasetFPathTxt <- 'Data/household_power_consumption.txt'
-datasetFPathZip <- 'Data/household_power_consumption.zip'
-datasetSrcURL <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+library(readr)
+library(dplyr)
+library(magrittr)
+library(lubridate)
 
-if (!file.exists('Data/household_power_consumption.txt')) {
-  if (!file.exists('Data/household_power_consumption.zip')) {
-    download.file(datasetSrcURL, datasetFPathZip)
+txtFPathDefault <- 'Data/household_power_consumption.txt'
+zipFPathDefault <- 'Data/household_power_consumption.zip'
+zipSrcURL <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+
+CheckOrDlData <- function(txtFPath, zipFPath=zipFPathDefault){
+  if (!file.exists(txtFPath)) {
+    if (!file.exists(zipFPath)) {
+      zipFPath <- zipFPathDefault
+      download.file(zipSrcURL, zipFPath)
+    }
+    unzip(zipFPath, exdir = 'Data/', overwrite = T)
+    txtFPath <- txtFPathDefault
+    if (file.exists(txtFPath)) {
+      return(txtFPath)
+    } else {
+      print(paste0('Error: extracting ', zipFPath, ' FAILED'))
+      return(FALSE)
+    }
+  } else {
+    return(txtFPath)
   }
-  unzip(datasetFPathZip, exdir = 'Data/', overwrite = T)
 }
 
+ParseData <- function(txtFPath) {
+  validFPath <- CheckOrDlData(txtFPath)
+  dataTmp <- read.table(validFPath, header = T, sep = ';')
+  data <- read_delim(validFPath, delim = ';', col_names = T)
+  data$Date %<>% dmy()
+  data$Time <- hms(dataTmp$Time)
+  return(data)
+}
+
+GetData <- function(txtFPath = txtFPathDefault, allDates = F) {
+  data <- ParseData(txtFPath)
+  if (allDates) {
+    return(data)
+  } else {
+    data %>%
+      filter(Date >= date('2007-02-01'), Date <= date('2007-02-02')) %>%
+      return()
+  }
+}
