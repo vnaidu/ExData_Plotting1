f807fbe6e259bda0ec0f5376a4ef0c0c8b88ed62
Victor Naidu <vnaidu114@gmail.com>
2016-07-16 03:44:23 -0700

    Add comments and remove library(dplyr)

--- plot1.R
+++ plot1.R
@@ -1,14 +1,18 @@
 source("dataHelper.R")
 source("plotHelper.R", local = TRUE)
-library(dplyr)
 
 pwrc <- GetData()
 
-startDev('plot1')
+# Start plotting device using default options
+startDev("plot1")
+
+# Calculate date start/end for graph
 hist(
   pwrc$Global_active_power,
   main = "Global Active Power",
   col = "red",
   xlab = "Global Active Power (kW)"
 )
+
+# Close plotting device
 closeDev()
